5d43623b1d2e4f4db3e0e8835b5aef6a89b39559
이경범 <tkdwk567@naver.com>
2022-03-27 15:44:32 +0900

    Initial commit

---
+++ src/main/java/com/example/boardspringbootwebservice/BoardSpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardspringbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardSpringbootWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardSpringbootWebserviceApplication.class, args);
+    }
+
+}
