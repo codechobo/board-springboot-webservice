fdc373adeac4b9d5a07e61042966e0fac34bb381
이경범 <tkdwk567@naver.com>
2022-03-30 18:21:38 +0900

    Revert "Revert "yml -> properties 로 변경 및 출력 SQL문 h2 SQL 쿼리문 MySQL 문법으로 변경""
    This reverts commit 2aecda059759fdc8efdf96750edabdf5e441c5dc.

---
+++ src/main/java/com/example/boardspringbootwebservice/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.example.boardspringbootwebservice.service.posts;
+
+import com.example.boardspringbootwebservice.domain.posts.PostsRepository;
+import com.example.boardspringbootwebservice.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
