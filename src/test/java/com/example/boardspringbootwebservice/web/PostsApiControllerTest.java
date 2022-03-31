98d78d9b2bd2124c9718328c8e57790a5f984cb0
이경범 <tkdwk567@naver.com>
2022-03-31 13:27:16 +0900

    findById() 기능 테스트

--- src/test/java/com/example/boardspringbootwebservice/web/PostsApiControllerTest.java
+++ src/test/java/com/example/boardspringbootwebservice/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.example.boardspringbootwebservice.web;
 import com.example.boardspringbootwebservice.domain.posts.Posts;
 import com.example.boardspringbootwebservice.domain.posts.PostsRepository;
 import com.example.boardspringbootwebservice.web.dto.PostsSaveRequestDto;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -17,6 +20,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Log4j2
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PostsApiControllerTest {
 
@@ -30,13 +34,13 @@ class PostsApiControllerTest {
     private PostsRepository postsRepository;
 
     @AfterEach
-    public void clean() {
+    public void afterEach() {
         postsRepository.deleteAll();
     }
 
     @Test
     @DisplayName("Posts_등록_TEST")
-    void PostsApiControllerTest() {
+    void save() {
         // given
         PostsSaveRequestDto dto = PostsSaveRequestDto.builder()
                 .title("테스트")
@@ -58,5 +62,35 @@ class PostsApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(dto.getTitle());
         assertThat(all.get(0).getContent()).isEqualTo(dto.getContent());
         assertThat(all.get(0).getAuthor()).isEqualTo(dto.getAuthor());
+    }
+
+    @Test
+    @DisplayName("findByID_조회_TEST")
+    void findById() {
+        // given
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title("테스트")
+                .content("테스트 중 입니다.")
+                .author("홍길동")
+                .build();
+
+        Posts savedPosts = postsRepository.save(requestDto.toEntity());
+
+        Long findId = 1L;
+        String url = "http://localhost:" + port + "/api/v1/posts/" + findId;
+
+
+        HttpEntity<PostsSaveRequestDto> postsSaveRequestDtoHttpEntity =
+                new HttpEntity<>(requestDto);
+
+        // when
+        ResponseEntity<Posts> responseEntity =
+                restTemplate.exchange(url, HttpMethod.GET, postsSaveRequestDtoHttpEntity, Posts.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody().getTitle()).isEqualTo(requestDto.getTitle());
+        assertThat(responseEntity.getBody().getContent()).isEqualTo(requestDto.getContent());
+        assertThat(responseEntity.getBody().getAuthor()).isEqualTo(requestDto.getAuthor());
     }
 }
