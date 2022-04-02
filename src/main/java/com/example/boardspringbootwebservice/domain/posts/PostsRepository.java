c0a316f67ed46109e43a2f17372f4aab89e171fb
이경범 <tkdwk567@naver.com>
2022-04-02 18:38:17 +0900

    PostsRepository findAllDesc() 기능 생성

--- src/main/java/com/example/boardspringbootwebservice/domain/posts/PostsRepository.java
+++ src/main/java/com/example/boardspringbootwebservice/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.example.boardspringbootwebservice.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // JpaRepository 를 상속하여 인터페이스를 생성해줍니다. 제네릭 타입으로는 <T, ID> 설정합니다.
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("select p from Posts p order by p.id desc")
+    List<Posts> findAllDesc();
 }
