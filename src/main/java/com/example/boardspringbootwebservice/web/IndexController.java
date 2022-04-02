8abcb527aae94289e9cdf052da6f9522da7e650b
이경범 <tkdwk567@naver.com>
2022-04-02 20:25:22 +0900

    model 핸들러 생성

--- src/main/java/com/example/boardspringbootwebservice/web/IndexController.java
+++ src/main/java/com/example/boardspringbootwebservice/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.example.boardspringbootwebservice.web;
 
+import com.example.boardspringbootwebservice.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
@@ -15,5 +22,6 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
+
 
 }
